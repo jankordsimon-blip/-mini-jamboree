d285a04063f0fc4c79f1df751efa1e041006c482
Simon Jankord <simonjankord@MacBook-Air-von-Simon-3.local>
2026-03-23 15:02:03 +0100

    fix file locations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+  site: 'https://jankordsimon-blip.github.io',
+  base: '/-mini-jamboree',
 });
--- src/config/astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://jankordsimon-blip.github.io',
-  base: '/-mini-jamboree',
-});
