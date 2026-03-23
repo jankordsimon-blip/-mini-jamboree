97f9b04b8121b67c9333d54a06a5c7372e49fdf1
Simon Jankord <simonjankord@MacBook-Air-von-Simon-3.local>
2026-03-23 15:08:38 +0100

    add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://jankordsimon-blip.github.io',
   base: '/-mini-jamboree',
+  integrations: [react()],
 });
