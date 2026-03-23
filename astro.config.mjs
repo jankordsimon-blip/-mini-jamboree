6b578f5ed06e24a9aafd9de5f5465ccc4a9ec5fb
Simon Jankord <simonjankord@MacBook-Air-von-Simon-3.local>
2026-03-23 16:44:54 +0100

    fix all routing for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   site: 'https://jankordsimon-blip.github.io',
-  base: '/-mini-jamboree',
-  integrations: [react()],
+  base: isProd ? '/-mini-jamboree' : '/',
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
 });
