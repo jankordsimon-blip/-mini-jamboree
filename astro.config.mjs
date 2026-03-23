c58e8b4a3b7f722098426d2ae54f3c55f9679e4c
Simon Jankord <simonjankord@MacBook-Air-von-Simon-3.local>
2026-03-23 17:15:13 +0100

    switch config back to Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
-  site: 'https://jankordsimon-blip.github.io',
-  base: isProd ? '/mini-jamboree' : '/',
-
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react()]
 });
